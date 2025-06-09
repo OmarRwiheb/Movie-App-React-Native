0f53b42184e8772220b8f00ca7ce7283ddd49482
OmarRwiheb <59856062+OmarRwiheb@users.noreply.github.com>
2025-06-09 17:27:03 +0300

    Initial commit

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Saved = () => {
+  return (
+    <View className="bg-primary flex-1 justify-center items-center">
+      <Text>saved</Text>
+    </View>
+  );
+};
+
+export default Saved;
